2894ff00c070afed3bc9a4b83087eec4e8452958
sachacha <37576654+sachacha@users.noreply.github.com>
2019-11-13 17:05:04 +0100

    with handlefunc out of infinite for

--- main.go
+++ main.go
@@ -16,19 +16,19 @@ func main() {
 		port = "8080"
 	}
 
+	http.HandleFunc("/repocheck/v1/commits", APIs.HandlerCommits)
+	http.HandleFunc("/repocheck/v1/languages", APIs.HandlerLanguages)
+	http.HandleFunc("/repocheck/v1/webhooks/", APIs.HandlerWebhookWithId)
+	http.HandleFunc("/repocheck/v1/webhooks", APIs.HandlerWebhook)
+	http.HandleFunc("/repocheck/v1/status", APIs.HandlerStatus)
+	fmt.Println("Listening on port " + port)
+
 	var ticker = time.Now()
 
 	for {
 		if time.Since(ticker).Seconds() >= 10 {
 			fmt.Println("Done !!")
 			ticker = time.Now()
-		}else {
-			http.HandleFunc("/repocheck/v1/commits", APIs.HandlerCommits)
-			http.HandleFunc("/repocheck/v1/languages", APIs.HandlerLanguages)
-			http.HandleFunc("/repocheck/v1/webhooks/", APIs.HandlerWebhookWithId)
-			http.HandleFunc("/repocheck/v1/webhooks", APIs.HandlerWebhook)
-			http.HandleFunc("/repocheck/v1/status", APIs.HandlerStatus)
-			fmt.Println("Listening on port " + port)
 		}
 	}
 
