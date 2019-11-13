4236473c5ad07db03b35abbdf622359f0f54da95
sachacha <37576654+sachacha@users.noreply.github.com>
2019-11-13 16:30:05 +0100

    testing infinite loop

--- main.go
+++ main.go
@@ -20,5 +20,14 @@ func main() {
 	http.HandleFunc("/repocheck/v1/webhooks", APIs.HandlerWebhook)
 	http.HandleFunc("/repocheck/v1/status", APIs.HandlerStatus)
 	fmt.Println("Listening on port " + port)
+	
+	var START_TIME = time.Now()
+
+	for {
+		if (START_TIME.Second() > 10) {
+			fmt.Println("et de 10 !")
+		}
+	}
+	
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
